e701350747c8d8157972dd6c6cd9b06f6fcdae3c
ekitnat <32475089+ekitnat@users.noreply.github.com>
2019-03-28 12:17:34 -0400

    Update pyinit.sh

--- pyinit.sh
+++ pyinit.sh
@@ -1,6 +1,15 @@
 ## This is to install Python 3.8 version from source code, and install it on CentOS ##
+## 
+##
 
+##SSH to Linux then get to pull the code in raw
+##wget -O - https://raw.githubusercontent.com/ekitnat/Python/master/pyinit.sh | bash
+## the above command is to get the raw content from that URL and pipe it to bash ##
+
+##this will install the development tools ##
 yum groupinstall -y "development tools"
+
+##this is to install other tools python needs as dependency for the build ##
 yum install -y \
   libffi-devel \
   zlib-devel \
@@ -16,7 +25,10 @@ yum install -y \
   xz-devel \
   expat-devel
   
+  ## change directory ##
   cd /usr/src
+  
+  #get the python binary, extract it, and compile it, and instal it ##
  wget https://www.python.org/ftp/python/3.8.0/Python-3.8.0a3.tar.xz
  tar xf Python-3.8.0a3.tar.xz
  cd Python-3.8.0a3
